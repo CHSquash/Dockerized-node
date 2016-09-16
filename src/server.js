9b5a8d5d5b1be40db257c6c1df0790cfc162f569
chungho <chungho.song.582@gmail.com>
2016-09-15 18:56:39 -0700

    add error handling when mysql server disconnected

--- src/server.js
+++ src/server.js
@@ -1,13 +1,36 @@
 var mysql      = require('mysql');
 // host should be localhost for local development env
 // use nginx port mapping later.
-var config = require('./config.js')
-var connection = mysql.createConnection(config);
+var db_config = require('./config.js')
 
 var cors = require('cors');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+
+
+  connection.connect(function(err) {
+    if(err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    } else {
+      console.log('connected to the db')
+    }
+  });
+
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST')
+      handleDisconnect();
+    else
+      throw err;                                 
+  });
+}
+
+handleDisconnect();
 
 app.use(cors());
 app.use(bodyParser.json()); // support json encoded bodies
